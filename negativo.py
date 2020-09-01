db4eb2171147bfa0a6b6afde30d77a6841e0331d
Sarai Cruz <saraicruzarroyo@outlook.es>
2020-08-31 19:48:56 -0500

    contador de numeros hasta que sea negativo

---
+++ negativo.py
@@ -0,0 +1,10 @@
+numero = int (input ())
+contadorDePares = 0
+
+while numero >= 0:
+    if numero % 2 ==0:
+        contadorDePares += 1
+    numero = int (input ())
+
+print ( "Total de pares=",contadorDePares,sep = "")
+
