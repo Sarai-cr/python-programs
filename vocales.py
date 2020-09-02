e2c1bd88fab4ec93d419c2e8f519388a5c0c1056
Sarai Cruz <saraicruzarroyo@outlook.es>
2020-09-02 11:40:44 -0500

    programa que indica si la letra ingresada es vocal

---
+++ vocales.py
@@ -0,0 +1,9 @@
+def vocales (vocal):
+    
+    if vocal == "a" or vocal == "e" or vocal == "i" or vocal == "o" or vocal == "u":
+        return True
+    else:
+        return False
+
+vocal =  (input ())
+print ( vocales(vocal) ) 
